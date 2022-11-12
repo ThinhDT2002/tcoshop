53a0f5611d5c28e234427b98baadd669fead6981
CrisKhang <trank6531@gmail.com>
2022-11-12 19:12:20 +0700

    fix link href logout + findAll Orders

--- TCO_Shop/src/main/java/com/tcoshop/api/OrderDetailAPI.java
+++ TCO_Shop/src/main/java/com/tcoshop/api/OrderDetailAPI.java
@@ -1,7 +1,10 @@
 package com.tcoshop.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class OrderDetailAPI {
 	@PostMapping()
 	public Order create(@RequestBody JsonNode orderData) {
 		return orderService.create(orderData);
+	}
+	
+	@GetMapping()
+	public List<Order> getAll(){
+		return orderService.findAll();
 	}
 }
