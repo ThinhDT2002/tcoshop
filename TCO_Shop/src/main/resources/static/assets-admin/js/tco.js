2177113c45bac8394ed379ca12824ec42583d992
codeCuaAn <anfnguyen113@gmail.com>
2022-12-03 11:30:43 +0700

    fix popup
    fix popup thống kê admin

--- TCO_Shop/src/main/resources/static/assets-admin/js/tco.js
+++ TCO_Shop/src/main/resources/static/assets-admin/js/tco.js
@@ -253,11 +253,4 @@ $(document).ready(function (h) {
             });
         });
 });
-// POPUP
-    document.getElementById("button-popup").addEventListener("click",function(){
-        document.querySelector(".popup").style.display = "flex";
-    })
 
-document.querySelector(".close").addEventListener("click",function(){
-    document.querySelector(".popup").style.display = "none";
-})
