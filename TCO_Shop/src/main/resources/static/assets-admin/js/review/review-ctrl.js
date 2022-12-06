68dcb91dead5937c5b6a2eb8575310af1d5b442e
CrisKhang <trank6531@gmail.com>
2022-12-06 14:30:21 +0700

    load + xóa review

--- TCO_Shop/src/main/resources/static/assets-admin/js/review/review-ctrl.js
+++ TCO_Shop/src/main/resources/static/assets-admin/js/review/review-ctrl.js
@@ -1,8 +1,6 @@
 adminApp.controller("review-ctrl", function($scope, $http){
 	
 	$scope.revs = [];
-	$scope.form = [];
-	
 	
 	$scope.initialize = function(){
 		$http.get("/api/reviews").then(resp =>{
@@ -10,20 +8,68 @@ adminApp.controller("review-ctrl", function($scope, $http){
 		})
 		
 	}
+	/*
+	$scope.delete = function(rev){
+		$http.delete(`/api/reviews/${rev.id}`).then(resp => {
+			var index = $scope.revs.findIndex(r => r.id == rev.id);
+			$scope.revs.splice(index, 1);
+			alert("Xóa đánh giá thành công!");
+		}).catch(error => {
+			alert("Lỗi xóa đánh giá!");
+			console.log("Error", error);
+		});
+	}
+	*/
+	
 	
 	$scope.initialize();
 	
-	$scope.create = function(){
-		let rev = angular.copy($scope.form);
-		$http.post("/api/revies", rev).then(resp => {
-			$scope.revs.push(resp.data);
-			alert("Thêm đánh giá sản phẩm thành công!");
-			
-		}).catch(error => {
-			alert("Lỗi thêm mới đánh giá!");
-			console.log("Error", error);
-		});
-		
+	$scope.delete = function(reviewId){
+		let text = "Xóa đánh giá này ?";
+		if(confirm(text) == true){
+			$http({
+				url: "/api/reviews",
+				method: "DELETE",
+				params: {
+					reviewId: reviewId
+				}
+			}).then(resp => {
+				let index = $scope.revs.findIndex(r => r.id == reviewId);
+				$scope.revs.splice(index, 1);
+				alert("Xóa đánh giá thành công!");
+			}).catch(error => {
+				alert("Lỗi xóa đánh giá!");
+				console.log("Error", error);
+			});
+		}
 	}
 	
+	$scope.reviewProperty = 'id';
+	$scope.sort = function(){
+		
+	}
+	$scope.currentPage = 0;
+	$scope.pageSize = '5';
+	
+	$scope.totalQuantity = function(){
+		return $scope.revs.length;
+	}
+	
+	$scope.numberOfPages = function(){
+		return Math.ceil($scope.revs.length / $scope.pageSize);
+	}
+	
+	for(let i= 0; i< 45; i++){
+		$scope.revs.push("Item " + i);
+	}
+	
+	$scope.pagination = function(){
+		$scope.currentPage = 0;
+	}
 })
+adminApp.filter('startFrom', function() {
+	return function(input, start) {
+		start = +start;
+		return input.slice(start);
+	}
+});
